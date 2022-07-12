e4cb12a749b58327727619d05261174becdd9ffc
David <Rigby335@hotmail.com>
2022-07-12 08:06:14 -0700

    Player.js functionality

--- src/Player.js
+++ src/Player.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import CardList from './CardList';
+import { useGameContext } from './GameProvider';
 
-export default function Player({ player, setTo, hand, setFrom, setSelectedCard, selectedCard, to }) {
+export default function Player({ player, hand }) {
+
+  const {
+    setFrom, setSelectedCard, selectedCard, to, setTo
+  } = useGameContext();
+
   return (
     <div className={`player ${to === player ? 'selected-player' : ''}`} onClick={() => setTo(player)}>
       <p>Player {player}</p>
